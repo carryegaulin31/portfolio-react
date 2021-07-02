9035dd56e61deabe0a3952ea3a7f42fa1254a169
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-07-01 23:20:07 -0400

    updated testimonials and photos

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -6,14 +6,12 @@ export default function About() {
           <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                Hi, I'm Reed.
-                <br className="hidden lg:inline-block" />I love to build amazing
+              Hello and welcome! I'm Carrye.
+                <br className="hidden lg:inline-block" /> I love to build amazing
                 apps.
               </h1>
               <p className="mb-8 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                Laborum, voluptas natus?
+               Let me start off by introducing myself. My name is Carrye Gaulin and I am originally from Bangor, Maine. Don't let that fool you! I have ventured to farther off places. I have spent time living in various locations which include The U.S. Virgin Islands, the Florida Keys and the beaches of North Carolina. I absolutely love traveling to new places on exciting adventures. I love the sun, sand and absolutly any kind of fishing. Sounds nothing like sitting in front of a computer and creating does it? I have been interested in computers and what they can do since the first time I used one. Out of highschool I worked very hard to get into a computer science program at the University of Maine in Orono. Circumstances beyond my control abruptly removed me from that path. Full circle 20+ years later I have returned, this time however, with an extremely successful knack for sales coupled with the same passion for the pursuit of knowledge in the computer science field. The road is challenging but what fun is life without a good challenge right?
               </p>
               <div className="flex justify-center">
                 <a
@@ -32,7 +30,7 @@ export default function About() {
               <img
                 className="object-cover object-center rounded"
                 alt="hero"
-                src="./coding.svg"
+                src="./meReal.png"
               />
             </div>
           </div>
--- my-portfolio/src/data.js
+++ my-portfolio/src/data.js
@@ -4,16 +4,16 @@ export const projects = [
     subtitle: "Express, Node, MySQL",
     description:
       "API of select list of the all time greats. Accesibility to novels, authors or genres and any combination in between. Fuzzy search available for titles and authors.",
-    image:"../public/novels.gif",
+    image: "./novels.gif",
     link: "http://novelsapi-env.eba-uftmpdwp.us-east-2.elasticbeanstalk.com/",
   },
   {
-    title: "React Tracks",
-    subtitle: "",
+    title: "DeisHacks",
+    subtitle: "Built with React Native, Express, MySQL and Nodemon",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "",
-    link: "",
+      "Collaborated with a team of six to build a mobile application for the Boston Police Teen Academy to keep teens and mentors connected in a safe space during and after the programas well as giving access to one-on-one support, community mental and physical health and events. Worked closely with client/founder to diagnose and prioritize features and services that should be included in the final product. Application built from idea to MVP in 48 hours. My primary contribution was backend. Final product exceeded the expectations of the client as well as won a top prize in the hack-a-thon.",
+    image: "./BTPA.gif",
+    link: "https://youtu.be/8tXSfM07E7E",
   },
   {
     title: "",
@@ -36,17 +36,17 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
+      "Carrye's greatest virtues undoubtedly are her team leadership and morale building skills in addition to her ability to maintain utmost professional behavior under high pressure and stressful situations. Furthermore, Carrye is also a fast and adaptable learner, takes initiative with very little instruction, dependable, honest, and an all around exceptional person.  She was an absolute joy to work with. She would be a tremendous asset to any company and has my highest recommendation. ",
+    image: "./TJ1.jpg",
+    name: "T.J. Naylor",
+    company: "General Manager, Hog Heaven, Islamorada, FL",
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
+      "I believe that having a background in hospitality transcends into many other fields as a major focal point in interacting with guest/client satisfaction as well as working and contributing to a team, which Carrye excels at. She always fulfilled her responsibilities and met or beat any expectations. Carrye is an asset to any company as she is great under pressure, a fast learner, hard working, takes direction well, takes initiative,is dependable, honest and an all around good person.",
+    image: "./jason1.jpg",
+    name: "Jason White",
+    company: "Director of Operations, Hill Restaurant Group, Washington DC",
   },
 ];
 
